cdc50d348aa589189d70d63c8210c08665950ed9
saltq <91617568+saltq144@users.noreply.github.com>
2022-06-14 15:20:46 +0300

    endToken

--- pastaignore.cpp
+++ pastaignore.cpp
@@ -45,8 +45,39 @@ int main() {
 	std::string midToken = "";
 	for (int i = j; i < input.size(); i++) {
 		if ((input[i] != ' ') && (input[i] != '\n') && (input[i] != '\r') && (input[i] != '\"')) midToken += input[i];
-		if (midToken == "not") { ret.push_back(midToken); break;}
+		if (midToken == "not") { ret.push_back(midToken); j = i; break;}
+		j = i;
 	}
 	std::cout << midToken << "\n";
+	//std::cout << input.substr(j, input.size() - j - 2);
+	startIndex = j;
+        endIndex   = -1;
+	//std::cout << input << "\n";
+	//std::cout << j << "\n";
+	bool inside = false;
+	std::string endToken = "";
+        for (int i = j; i < input.size(); i++) {
+		if (inside) endToken += input[i];
+                if (input[i] == '\"' && input[i-1] != '\\') {
+                        /*if (i == 0) {
+                                startIndex = 0;
+                                i++;
+                                continue;
+                        } else if (inside) {
+                                if (input[i - 1] == '\\') {
+                                        i++;
+                                        continue;
+                                } else {
+                                        endIndex = i;
+                                        j = i;
+                                        break;
+                                }
+                        } else if (!inside) { inside = true; std::cout << "INSIDE ENABLED!" << input[i];}*/
+		inside = !inside;
+                }
+        }
+	endToken = endToken.substr(0, endToken.size() - 1);
+	std::cout << endToken;
+	//std::cout << input.substr(startIndex + 1, endIndex - startIndex - 1);
 }
 
